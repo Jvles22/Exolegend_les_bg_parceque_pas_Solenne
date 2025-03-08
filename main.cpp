28df09953f9d3f204d0139d215749243ab9b0727
Jvles22 <desaintjanjules@gmail.com>
2025-03-08 03:13:32 +0100

    Ferdi casse les couilles

--- main.cpp
+++ main.cpp
@@ -1,53 +1,153 @@
 #include "gladiator.h"
+#include <cmath>
+#undef abs
+
+// x,y représentent des coordonnées en m
+// Vector{1.5,1.5} représente le point central
+// Pour convertir une cordonnée de cellule (i,j) (0<=i<=13, 0<=j<=13) :
+// x = i * CELL_SIZE + 0.5*CELL_SIZE
+// y = j * CELL_SIZE + 0.5*CELL_SIZE
+// avec CELL_SIZE = 3.0/14 (~0.214)
+
+class Vector2
+{
+  public:
+    Vector2() : _x(0.), _y(0.)
+    {
+    }
+    Vector2(float x, float y) : _x(x), _y(y)
+    {
+    }
+
+    float norm1() const
+    {
+        return std::abs(_x) + std::abs(_y);
+    }
+    float norm2() const
+    {
+        return std::sqrt(_x * _x + _y * _y);
+    }
+    void normalize()
+    {
+        _x /= norm2();
+        _y /= norm2();
+    }
+    Vector2 normalized() const
+    {
+        Vector2 out = *this;
+        out.normalize();
+        return out;
+    }
+
+    Vector2 operator-(const Vector2 &other) const
+    {
+        return {_x - other._x, _y - other._y};
+    }
+    Vector2 operator+(const Vector2 &other) const
+    {
+        return {_x + other._x, _y + other._y};
+    }
+    Vector2 operator*(float f) const
+    {
+        return {_x * f, _y * f};
+    }
+
+    bool operator==(const Vector2 &other) const
+    {
+        return std::abs(_x - other._x) < 1e-5 && std::abs(_y - other._y) < 1e-5;
+    }
+    bool operator!=(const Vector2 &other) const
+    {
+        return !(*this == other);
+    }
+
+    float x() const
+    {
+        return _x;
+    }
+    float y() const
+    {
+        return _y;
+    }
+
+    float dot(const Vector2 &other) const
+    {
+        return _x * other._x + _y * other._y;
+    }
+    float cross(const Vector2 &other) const
+    {
+        return _x * other._y - _y * other._x;
+    }
+    float angle(const Vector2 &m) const
+    {
+        return std::atan2(cross(m), dot(m));
+    }
+    float angle() const
+    {
+        return std::atan2(_y, _x);
+    }
+
+  private:
+    float _x, _y;
+};
+
 Gladiator *gladiator;
-
-float kw = 1.2;
-float kv = 2.f;
-float wlimit = 3.f;
-float vlimit = 0.6;
-float erreurPos = 0.07;
-float casesize = 0.25;
-double reductionAngle(double x)
-{
-    x = fmod(x + PI, 2 * PI);
-    if (x < 0)
-        x += 2 * PI;
-    return x - PI;
-}
-void go_to(Position cons, Position pos)
-{
-    double consvl, consvr;
-    double dx = cons.x - pos.x;
-    double dy = cons.y - pos.y;
-    double d = sqrt(dx * dx + dy * dy);
-
-    if (d > erreurPos)
-    {
-        double rho = atan2(dy, dx);
-        double consw = kw * reductionAngle(rho - pos.a);
-
-        double consv = kv * d * cos(reductionAngle(rho - pos.a));
-        consw = abs(consw) > wlimit ? (consw > 0 ? 1 : -1) * wlimit : consw;
-        consv = abs(consv) > vlimit ? (consv > 0 ? 1 : -1) * vlimit : consv;
-
-        consvl = consv - gladiator->robot->getRobotRadius() * consw; // GFA 3.6.2
-        consvr = consv + gladiator->robot->getRobotRadius() * consw; // GFA 3.6.2
-    }
-    else
-    {
-        consvr = 0;
-        consvl = 0;
-    }
-
-    gladiator->control->setWheelSpeed(WheelAxis::RIGHT, consvr, false); // GFA 3.2.1
-    gladiator->control->setWheelSpeed(WheelAxis::LEFT, consvl, false);  // GFA 3.2.1
-}
 
 void reset()
 {
-    // fonction de reset:
-    // initialisation de toutes vos variables avant le début d'un match
-    gladiator->log("Call of reset function"); // GFA 4.5.1
+}
+
+inline float moduloPi(float a) // return angle in [-pi; pi]
+{
+    return (a < 0.0) ? (std::fmod(a - M_PI, 2 * M_PI) + M_PI) : (std::fmod(a + M_PI, 2 * M_PI) - M_PI);
+}
+
+inline bool aim(Gladiator *gladiator, const Vector2 &target, bool showLogs)
+{
+    constexpr float ANGLE_REACHED_THRESHOLD = 0.1;
+    constexpr float POS_REACHED_THRESHOLD = 0.05;
+
+    auto posRaw = gladiator->robot->getData().position;
+    Vector2 pos{posRaw.x, posRaw.y};
+
+    Vector2 posError = target - pos;
+
+    float targetAngle = posError.angle();
+    float angleError = moduloPi(targetAngle - posRaw.a);
+
+    bool targetReached = false;
+    float leftCommand = 0.f;
+    float rightCommand = 0.f;
+
+    if (posError.norm2() < POS_REACHED_THRESHOLD) //
+    {
+        targetReached = true;
+    }
+    else if (std::abs(angleError) > ANGLE_REACHED_THRESHOLD)
+    {
+        float factor = 0.2;
+        if (angleError < 0)
+            factor = -factor;
+        rightCommand = factor;
+        leftCommand = -factor;
+    }
+    else
+    {
+        float factor = 0.5;
+        rightCommand = factor; //+angleError*0.1  => terme optionel, "pseudo correction angulaire";
+        leftCommand = factor;  //-angleError*0.1   => terme optionel, "pseudo correction angulaire";
+    }
+
+    gladiator->control->setWheelSpeed(WheelAxis::LEFT, leftCommand);
+    gladiator->control->setWheelSpeed(WheelAxis::RIGHT, rightCommand);
+
+    if (showLogs || targetReached)
+    {
+        gladiator->log("ta %f, ca %f, ea %f, tx %f cx %f ex %f ty %f cy %f ey %f", targetAngle, posRaw.a, angleError,
+                       target.x(), pos.x(), posError.x(), target.y(), pos.y(), posError.y());
+    }
+
+    return targetReached;
 }
 
 void setup()
@@ -55,88 +155,38 @@ void setup()
     // instanciation de l'objet gladiator
     gladiator = new Gladiator();
     // enregistrement de la fonction de reset qui s'éxecute à chaque fois avant qu'une partie commence
-    gladiator->game->onReset(&reset); // GFA 4.4.1
+    gladiator->game->onReset(&reset);
 }
+double calculateDistance(const Position& coord1, const Coordinate& coord2) {
+    return std::sqrt(std::pow(coord1.x - coord2.x, 2) + std::pow(coord1.y - coord2.y, 2));
+}
+
+// Fonction principale qui prend une liste de listes de coordonnées et une position
+void processCoordinates(const std::vector<std::vector<Position>>& PositionLists, Position positiongladiator) {
+    for (i=0;i<gladiator->maze->getSize;i++) {
+        for (j=0;j<gladiator->maze->getSize;j++) {
+            if(gladiator.maze.getCoins{i,j} !=null)
+            double distance = calculateDistance(Position, position);
+            std::cout << "Coordonnée: (" << Position.x << ", " << Position.y << "), ";
+            std::cout << "Distance par rapport à la position: " << distance << std::endl;
+        }
+    }
+}
+
 
 void loop()
 {
+
     if (gladiator->game->isStarted())
-    { // tester si un match à déjà commencer
-        // code de votre stratégie
-        Position myPosition = gladiator->robot->getData().position;
-        // Position goal{0.25, 0.25, 0};
-        // for(int i = 0; i < 5; i++){
-        Position goal{myPosition.x, myPosition.y, 0};
-        go_to(goal, myPosition);
-        //     go_to(goal, myPosition);
-        gladiator->log("Position : %f %f %f", myPosition.x, myPosition.y, myPosition.a); // GFA 4.5.1
-        // }
-        const MazeSquare *nearestSquare = gladiator->maze->getNearestSquare(); // GFA 4.7.2
-        // vérifier s'il a un mur au dessus
-        if (nearestSquare->northSquare != NULL)
+    {
+        static unsigned i = 0;
+        bool showLogs = (i % 50 == 0);
+
+        if (aim(gladiator, {1.5, 1.5}, showLogs))
         {
-            MazeSquare *topSquare = nearestSquare->northSquare;
-            // calculons les coordonnées du centre de cette case
-            float squareSize = gladiator->maze->getSquareSize(); // largeur d'une case GFA 4.7.4
-            Position centerCoor;
-            // pour calculer les coordonnées x et y il faut récupérer les index i et j de la case
-            centerCoor.x = (topSquare->i + 0.25) * squareSize;
-            centerCoor.y = (topSquare->j + 0.25) * squareSize;
-            // Position goal{centerCoor.x, centerCoor.y, 0};
-            go_to(centerCoor,myPosition);
+            gladiator->log("target atteinte !");
         }
-
-        
-        // vérifier s'il a un mur en dessous
-        else if (nearestSquare->southSquare != NULL){
-            MazeSquare *downSquare = nearestSquare->southSquare;
-            // calculons les coordonnées du centre de cette case
-            float squareSize = gladiator->maze->getSquareSize(); // largeur d'une case GFA 4.7.4
-            Position centerCoor;
-            // pour calculer les coordonnées x et y il faut récupérer les index i et j de la case
-            centerCoor.x = (downSquare->i + 0.25) * squareSize;
-            centerCoor.y = (downSquare->j + 0.25) * squareSize;
-            // Position goal{centerCoor.x, centerCoor.y, 0};
-            go_to(centerCoor,myPosition);
-
-        }
-        
-        // vérifier s'il a un mur à gauche
-        else if (nearestSquare->westSquare != NULL){
-            MazeSquare *leftSquare = nearestSquare->westSquare;
-            // calculons les coordonnées du centre de cette case
-            float squareSize = gladiator->maze->getSquareSize(); // largeur d'une case GFA 4.7.4
-            Position centerCoor;
-            // pour calculer les coordonnées x et y il faut récupérer les index i et j de la case
-            centerCoor.x = (leftSquare->i + 0.25) * squareSize;
-            centerCoor.y = (leftSquare->j + 0.25) * squareSize;
-            // Position goal{centerCoor.x, centerCoor.y, 0};
-            go_to(centerCoor,myPosition);
-
-        }
-        
-        // vérifier s'il a un mur à droite
-        else if (nearestSquare->eastSquare != NULL){
-            MazeSquare *rightSquare = nearestSquare->eastSquare;
-            // calculons les coordonnées du centre de cette case
-            float squareSize = gladiator->maze->getSquareSize(); // largeur d'une case GFA 4.7.4
-            Position centerCoor;
-            // pour calculer les coordonnées x et y il faut récupérer les index i et j de la case
-            centerCoor.x = (rightSquare->i + 0.25) * squareSize;
-            centerCoor.y = (rightSquare->j + 0.25) * squareSize;
-            // Position goal{centerCoor.x, centerCoor.y, 0};
-            go_to(centerCoor,myPosition);
-
-        }
-
-        // vérifier s'il a un mur à droite
-        // else if (nearestSquare->eastSquare == NULL)
-        // {
-        //     // SI le pointeur pointant vers la case droite est nul, cela veut dire qu'il y a un mur qui empêche de passer
-        //     gladiator->log("Il y a un mur à droite de la case où se trouve le robot"); // GFA 4.5.1
-        // }
-        
-
+        i++;
     }
-    delay(10);
+    delay(10); // boucle à 100Hz
 }
